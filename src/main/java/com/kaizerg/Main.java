a2b9a0f845398d9a4d1b80569b728f93b8359ad9
KaiZerg <gabitov.tatarin@gmail.com>
2023-07-31 16:47:07 +0500

    Update Main

--- src/main/java/com/kaizerg/Main.java
+++ src/main/java/com/kaizerg/Main.java
@@ -3,6 +3,45 @@ package com.kaizerg;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        byte varByte = 0;  // [-128, 127]
+        short varShort = 0; // [-32.768, 32.767]
+        int varInt = 0; // [-2.147.483.648, 2.147.483.647]
+        long varLong = 0; // [-9.223.372.036.854.775.808, 9.223.372.036.854.775.807]
+
+        int a = Integer.MAX_VALUE;
+        int b = 10;
+        System.out.println("Переполнение: " + (a + 1));
+
+        a = 12000;
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));
+
+        System.out.println("=============");
+
+        if (a > b) {
+            System.out.println("a + b = " + (a + b));
+        } else {
+            System.out.println("a - b = " + (a - b));
+        }
+
+        if (a > 0 && b < 100) {
+            System.out.println("a * b = " + (a * b));
+        }
+
+        a = -6523;
+        if (a > 0 || b < 100) {
+            System.out.println("a / b = " + (a / b));
+        }
+
+        System.out.println("=============");
+
+        double c = 32.4;
+        System.out.println("b + c = " + (c + b));
+        System.out.println("b - c = " + (b - c));
+        System.out.println("b * c = " + (b * c));
+        System.out.println("b / c = " + (b / c));
+        System.out.println(4 + 4.2);
     }
 }
